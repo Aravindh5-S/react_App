6bed80f57dc2fe3ff4f14509d69df4fdb295f327
Aravindh5-S <aravindhs5802@gmail.com>
2024-05-22 14:31:05 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/react-app" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/projects" element={<Projects />} />
